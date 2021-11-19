f2c150c4ff8dfc63e6027e5cab1ee9a6b34a8681
amitdekivadiya15 <amit.spyhunteritsolution@gmail.com>
2021-11-19 16:06:27 +0530

    uselocation

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 // const User = ({ match }) => {
 //   return <h1>This is {match.params.name} page </h1>;
@@ -7,8 +7,18 @@ import { useParams } from "react-router-dom";
 
 const User = () => {
   const { name } = useParams();
+  const location = useLocation();
+  console.log(location.pathname);
 
-  return <h1>This is {name} page </h1>;
+  return (
+    <>
+      <h1>This is {name} page </h1>
+      <p> My current Location is {location.pathname} </p>
+      {location.pathname == `/user/amit` ? (
+        <button onClick={() => alert("you are awesome")}> Click </button>
+      ) : null}
+    </>
+  );
 };
 
 export default User;
