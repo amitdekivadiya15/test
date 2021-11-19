272c0eb8f356582417a6bdcbf148750c62e39c09
amitdekivadiya15 <amit.spyhunteritsolution@gmail.com>
2021-11-19 14:52:00 +0530

    createnavbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Error from "./Components/Error";
+import Navbar from "./Components/Navbar";
 import { Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={About} />
         <Route path="/contact" component={Contact} />
