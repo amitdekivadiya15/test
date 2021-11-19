53f8ddebdbb4e5399b8b8048add3ccc2c26042a9
amitdekivadiya15 <amit.spyhunteritsolution@gmail.com>
2021-11-19 13:47:25 +0530

    routerbasic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import react from "react";
+import React from "react";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Error from "./Components/Error";
+import { Route, Switch } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
     <>
-      <h1> hello </h1>
+      <Switch>
+        <Route exact path="/" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route component={Error} />
+      </Switch>
+
+      {/* <About />
+      <Contact /> */}
     </>
   );
-}
+};
 
 export default App;
